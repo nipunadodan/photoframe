db3bb01794e4c66da6c6804b18206868e67098e7
Nipuna Dodantenna <nipunadodan@gmail.com>
2024-12-11 08:08:29 +0530

    Reverted to root

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'photowithframe',
+  base: '',
   plugins: [react(), tailwindcss()],
   /*css: {
     postcss: {
